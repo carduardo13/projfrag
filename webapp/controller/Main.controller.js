b0216fadbf8e210f0507f46c5bb167ad8ab63faf
Carlos Eduardo <carloseduardobs98@gmail.com>
2023-02-07 13:52:11 +0000

    Master

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -10,6 +10,29 @@ sap.ui.define([
         return Controller.extend("projfrag.controller.Main", {
             onInit: function () {
 
+            },
+
+            _getDialog: function() {
+                if (!this._oDialog) {
+                    this._oDialog = sap.ui.xmlfragment("projfrag.view.FragDialo", this);
+                    this.getView().addDependent(this._oDialog);
+                }
+
+                return this._oDialog
+            },
+
+
+            onShowDialog: function () {
+                this._getDialog().open()
+            },
+
+            onCloseDialog: function () {
+                this._getDialog().close()
             }
+
+
+
+
+
         });
     });
